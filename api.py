e1444132a52ef028366f50cfb44d18d2156b19fe
vinicius françozo <v.francozo@hotmail.com>
2022-09-21 17:12:52 -0300

    segundo commit

--- api.py
+++ api.py
@@ -5,16 +5,15 @@ app = FastAPI()
 
 
 class Produtos(BaseModel):
-    id: int
     q: str
     valor_minimo: int
     valor_maximo: int
 
 
 banco_de_dados = [
-    Produtos(id=1, q='teste', valor_minimo=10, valor_maximo=100),
-    Produtos(id=2, q='teste 2', valor_minimo=10, valor_maximo=100),
-    Produtos(id=3, q='teste 3', valor_minimo=10, valor_maximo=100)
+    Produtos(q='teste', valor_minimo=10, valor_maximo=100),
+    Produtos(q='teste 2', valor_minimo=10, valor_maximo=100),
+    Produtos(q='teste 3', valor_minimo=10, valor_maximo=100)
 ]
 
 
